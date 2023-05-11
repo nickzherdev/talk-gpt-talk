c81d24874e353a149e60c421296d836ddb016c42
Nikolay Zherdev <nikolay.zherdev@evocargo.com>
2023-05-11 21:32:20 +0300

    NEW: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # talk-gpt-talk
 Split traffic tunneling to get access to both work resources and restricted sites
+
+## Clone this repo
+
+    git clone https://github.com/nickzherdev/talk-gpt-talk.git
+
+## Add environment variables to ~/.zshrc (example)
+
+    export MY_GATEWAY="192.168.2.1"
+    export MY_INET_IFACE="wlp0s20f3"
+    export MY_VPN_DOMAINS="mx.evocargo.com wiki.evocargo.org jira.evocargo.org git.evocargo.org id.evocargo.org sso.evocargo.site cloud.evocargo.org"
+    export VATS_DOMAIN="172.25.192.0/24"
+
+## Add symlink to /usr/local/bin for convinience
+
+    sudo ln -s <full_path>/talk-gpt-talk/talk-gpt.sh /usr/local/bin/talk-gpt.sh
+
+## Ad permission to execute
+
+    sudo chmod +x /usr/local/bin/talk-gpt.sh
+
+## (optional) Check that link was created successfully (should be green/blue)
+
+    ls -la /usr/local/bin
+
+## (optional) To prevent entering a password each time, add an exeption to sudoers
+
+    YOUR_USER_NAME ALL=(ALL) NOPASSWD: /usr/local/bin/talk-gpt.sh
+
+## Launch
+
+    source ~/.zshrc
+    talk-gpt.sh
---
+++ talk-gpt.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# List of domain names to route through the VPN
+domains=(${MY_VPN_DOMAINS})
+total=${#domains[@]}
+
+# Check if the current default route is through the non-VPN connection
+current_default=$(ip route show | grep -m1 '^default' | awk '{print $5}')
+if [ "$current_default" == "$MY_INET_IFACE" ]; then
+    # Change the default route back to the VPN
+    echo -e "${GREEN}Switching default route back to VPN...${NC}"
+    sudo ip route del default dev $MY_INET_IFACE
+    sudo ip route add default dev ppp0
+fi
+
+# remove any old rules for these domains
+echo -e "${RED}Removing old rules...${NC}"
+for domain in "${domains[@]}"; do
+    for ip in $(ip route show | grep -oP '(\d{1,3}\.){3}\d{1,3}(?= dev ppp0)'); do
+        sudo ip route del $ip dev ppp0
+    done
+done
+echo -e "${GREEN}Done.${NC}"
+
+# add a route to connect to VATS through the VPN
+echo -e "${RED}Adding route for VATS...${NC}"
+sudo ip route add "${VATS_DOMAIN}" dev ppp0
+
+# resolve current IP addresses for these domains
+echo -e "${RED}Adding new rules...${NC}"
+count=0
+for domain in "${domains[@]}"; do
+    echo -ne "${GREEN}Processing $domain ... (${count}/${total}) ${NC}\r"
+    for ip in $(getent ahostsv4 $domain | awk '{ print $1 }' | sort -u); do
+        sudo ip route add $ip dev ppp0 >/dev/null 2>&1
+    done
+    ((count++))
+done
+echo -e "${GREEN}Done. (${total}/${total})${NC}"
+
+# Change default route
+echo -e "${RED}Switching default route to non-VPN connection...${NC}"
+sudo ip route del default dev ppp0
+sudo ip route add default via "${MY_GATEWAY}" dev "${MY_INET_IFACE}"
+echo -e "${GREEN}Done.${NC}"
